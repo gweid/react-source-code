397235c4d97a360e934110bddf462702efec9fc7
guanwei <guanw01@mingyuanyun.com>
2024-09-26 17:31:49 +0800

    实现简单版的react.createElement

--- react-demo/src/index.js
+++ react-demo/src/index.js
@@ -1,7 +1,14 @@
-// import ReactDOM from 'react-dom/client';
+// import React from 'react';
+import React from './react'
+// import ReactDOM from 'react-dom'; // react16.x 版本
+// import ReactDOM from 'react-dom/client'; // react18.x 版本
 
+// react 18.x 版本
 // const root = ReactDOM.createRoot(document.getElementById('root'));
-
 // root.render(<div>react demo</div>); 
 
-console.log(<div>react demo</div>);
+// react16.x 版本
+// const root = document.getElementById('root')
+// ReactDOM.render(<div>react demo</div>, root)
+
+console.log(<div style={{color: 'red'}} key="once">react demo</div>);
---
+++ react-demo/src/react.js
@@ -0,0 +1,43 @@
+// 自定义 React.createElement 函数
+import { REACT_ELEMENT } from './constant'
+
+// React.createElement("div", null, "react demo");
+const createElement = (type, properties, ...args) => {
+
+  // 通过在 jsx 文件中 console.log(<div>react demo</div>) 得到的虚拟 DOM 如下：
+  // {
+  //   "$$typeof": Symbol(react.element),
+  //   "type": "div",
+  //   "key": null,
+  //   "ref": null,
+  //   "props": {
+  //       "children": "react demo"
+  //   },
+  //   "_owner": null, // 这个与 react 无关的，是 babel 转化带来的
+  //   "_store": {} // 这个与 react 无关的，是 babel 转化带来的
+  // }
+
+  // 先删除不是 react 本身的无用属性
+  const filterKey = ['_owner', '_store']
+  filterKey.forEach(key => {
+    delete properties[key]
+  })
+
+  const { key = null, ref = null,  ...props } = properties
+
+  props.children = args
+
+  return {
+    $$typeof: REACT_ELEMENT,
+    type: type,
+    key,
+    ref,
+    props
+  }
+}
+
+const React = {
+  createElement
+}
+
+export default React
