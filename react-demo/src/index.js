aa8a4e74d61b694305a01dea298d56d901715e2f
guanwei <guanw01@mingyuanyun.com>
2024-09-28 17:19:50 +0800

    手写react--优化代码

--- react-demo/src/index.js
+++ react-demo/src/index.js
@@ -11,13 +11,16 @@ import ReactDOM from './mini-react/react-dom';
 // react16.x 版本
 // const root = document.getElementById('root');
 // ReactDOM.render(<div key="once" ref="divBox" style={{color: '#333' }}><div><span style={{color: 'red', fontSize: '20px' }}>哈哈哈</span>子节点</div>react demo</div>, root);
-
+ 
 // 手动实现的 react
 const root = document.getElementById('root');
 
+// -------------------- 普通标签
 // ReactDOM.render(<div key="once" ref="divBox" style={{color: '#333' }}><div><span style={{color: 'red', fontSize: '20px' }}>哈哈哈</span>子节点</div>react demo</div>, root);
 // console.log(<div key="once" ref="divBox" style={{color: '#333' }}><div><span style={{color: 'red', fontSize: '20px' }}>哈哈哈</span>子节点</div>react demo</div>);
 
+
+// -------------------- 函数组件
 // const MyFuncCom = (props) => {
 //   return (
 //     <div key="once" ref="divBox" style={{color: '#333' }}><div><span style={{color: 'red', fontSize: '20px' }}>哈哈哈</span>子节点</div>react demo</div>
@@ -26,6 +29,8 @@ const root = document.getElementById('root');
 // ReactDOM.render(<MyFuncCom name='my-func-com' />, root);
 // console.log(<MyFuncCom name='my-func-com' />);
 
+
+// -------------------- 类组件
 class MyClassCom extends React.Component {
   constructor(props) {
     super(props)
