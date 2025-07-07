d0329538e789f134270b359c5f64dd79973935ae
guanwei <guanw01@mingyuanyun.com>
2025-07-07 22:48:44 +0800

    react源码---目录更改

--- react18-source-code-read/src/components/Num.js
+++
@@ -1,9 +0,0 @@
-export default function Num({ resource }) {
-  const num = resource.num.read();
-  return (
-    <div>
-      <h3>Num</h3>
-      <p>{num}</p>
-    </div>
-  );
-}
--- react18-source-code-read/src/components/User.js
+++
@@ -1,9 +0,0 @@
-export default function User({ resource }) {
-  const user = resource.user.read();
-  return (
-    <div>
-      <h3>User</h3>
-      <p>网络请求：{user.name.first}</p>
-    </div>
-  );
-}
